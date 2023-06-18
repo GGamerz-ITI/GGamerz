974dd005fd05be3aa5f472860ef3706e9e522a95
Shehab Hossam <Shehab.haz@gmail.com>
2023-06-18 10:51:39 +0300

    changed database code position

--- NodeJs/server.js
+++ NodeJs/server.js
@@ -9,13 +9,15 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cors());
 
+
+// Connecting to Database
+const connectDB = require(path.join(__dirname, "./db"));
+connectDB();
+
 // Required For Routers
 const UserRouter = require(path.join(__dirname ,"./routers/UserRouter"));
 
 // Routers
 app.use('/api/users',UserRouter);
 
-// Connecting to Database
-const connectDB = require(path.join(__dirname, "./db"));
-connectDB();
 
