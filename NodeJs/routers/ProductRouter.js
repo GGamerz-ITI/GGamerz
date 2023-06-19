313bc80b58becfc05d71d23dab7f044d8ccf65a2
asmaa <asmaagamal99192@gmail.com>
2023-06-19 13:46:23 +0300

    products router

---
+++ NodeJs/routers/ProductRouter.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const path = require("path");
+
+const productController = require(path.join(__dirname, "../controllers/ProductController"));
+
+router.get("/", productController.getAllProducts);
+router.get("/create", productController.createProduct);
+router.get("/:id", productController.getProductById);
+router.put("/:id", productController.updateProduct);
+router.delete("/:id", productController.deleteProduct);
