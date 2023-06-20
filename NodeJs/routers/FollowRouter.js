fc5bdd3d0b886bd4d6e63acc669429cd0e65cca9
Shehab Hossam <Shehab.haz@gmail.com>
2023-06-20 15:07:17 +0300

    added community follow/unfollow/get followers/get followings

--- NodeJs/routers/FollowRouter.js
+++ NodeJs/routers/FollowRouter.js
@@ -1,11 +1,15 @@
-// const express = require("express");
-// const router = express.Router();
-// const path = require("path");
+const express = require("express");
+const router = express.Router();
+const path = require("path");
+const { route } = require("./UserStatsRouter");
 
-// const followController = require(path.join(__dirname, "../controllers/FollowController"));
+const followController = require(path.join(__dirname, "../controllers/FollowController"));
 
-// router.put("/points", userStatsController.updatePoints);
 
-// router.put("/level", userStatsController.updateLevel);
+router.post('/follow',followController.follow);
+router.post('/unfollow',followController.unfollow);
 
-// module.exports = router;
+router.get('/:id/followers',followController.getUserFollowers); // Get User followers
+router.get('/:id/followings',followController.getUserFollowing); // Get People who are followed
+
+module.exports = router;
