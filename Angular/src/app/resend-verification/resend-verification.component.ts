625fe837f21edb8334bfae81c8e2690f2c75eb32
Mayar <mayarhamed15@gmail.com>
2023-06-21 22:09:09 +0300

    resend verification component

--- Angular/src/app/resend-verification/resend-verification.component.ts
+++ Angular/src/app/resend-verification/resend-verification.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { VerifyService } from '../services/verify.service';
+import { ToastrService } from 'ngx-toastr';
+import { error } from 'jquery';
 
 @Component({
   selector: 'app-resend-verification',
@@ -6,5 +9,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./resend-verification.component.css']
 })
 export class ResendVerificationComponent {
-
+  email = ""
+  constructor(private toastr: ToastrService, private verifyService: VerifyService) { }
+  send() {
+    console.log(this.email)
+    this.verifyService.sendVerification(this.email).subscribe({
+      next: () => {
+        this.toastr.success("Verification sent", "Feedback");
+        setTimeout(() => {
+          this.toastr.clear()
+        }, 3000);
+      },
+      error: (error) => {
+        this.toastr.error(error, "Error");
+        setTimeout(() => {
+          this.toastr.clear()
+        }, 3000);
+      }
+    })
+  }
 }
