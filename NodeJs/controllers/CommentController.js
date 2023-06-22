52fe0b2b45160ef13bb7249b48492750e6902b28
Shehab Hossam <Shehab.haz@gmail.com>
2023-06-22 17:30:22 +0300

    change bcrypt to bcryptjs package

--- NodeJs/controllers/CommentController.js
+++ NodeJs/controllers/CommentController.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const Joi = require("joi");
 const path = require("path");
 const User = require(path.join(__dirname,"../models/User.js"))
